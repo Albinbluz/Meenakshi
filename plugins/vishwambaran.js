e287c1af9d092407762134233a6c8b7b18c3cdfb
Vishnu Prasad <87913417+vishnuserkl29@users.noreply.github.com>
2021-10-25 00:25:47 +0530

    Update vishwambaran.js

--- plugins/vishwambaran.js
+++ plugins/vishwambaran.js
@@ -9,8 +9,8 @@ Meenu.addCommand({ pattern: 'owner ?(.*)', fromMe: false, desc: 'owner number' }
 //coded by Saidali കൊല്ലം ഫ്യുരി
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Vishnu [VISHNUSER]\n' // full name
-            + 'ORG:Vishnuser;\n' // 
+            + 'FN:Vishnu Prasad\n' // full name
+            + 'ORG:Carmel Polytechnic College;\n' // 
             + 'TEL;type=CELL;type=VOICE;waid=916235989299:+91 8891714036\n' // WhatsApp ID + phone number
             + 'END:VCARD'
 await message.client.sendMessage(message.jid,{displayname: "Vishnu [VISHNUSER]", vcard: vcard}, MessageType.contact)
